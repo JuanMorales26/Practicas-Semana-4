334094494a5b410396dffa2b0621dd60b4ad5640
Juan Morales <launchx10261@innovaccion.mx>
2022-05-11 00:10:16 -0500

    Agregando exitosamente pruebas de Reader

--- 1_Refactoring/lib/utils/Reader.js
+++ 1_Refactoring/lib/utils/Reader.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 
 class Reader{
-    static readJsonFile(path){
+    static readJSONFile(path){
         const rawdata = fs.readFileSync(path);
         const explorers = JSON.parse(rawdata);
         return explorers;
